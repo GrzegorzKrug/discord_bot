bad4588919cf35d7ce8ae18ff22894129069ee52
Greg <grzesiekk321@gmail.com>
2020-09-21 11:03:09 +0200

    Sending notifications in other channel

--- bot/bot_modules/bot.py
+++ bot/bot_modules/bot.py
@@ -350,7 +350,7 @@ async def on_message(message):
 async def on_ready():
     act = Activity(type=ActivityType.watching, name=" if you need !help")
     await bot.change_presence(status=Status.idle)
-    await _announcement([750696820736393261], "✅ Hey, i'm now online.")
+    await _announcement([755715591339376662], "✅ Hey, i'm now online.")
     await bot.change_presence(activity=act, status=Status.online)
     logger.warning(f"On ready announcement is constant")
     logger.info(f"Going online as {bot.user.name}")
@@ -360,7 +360,7 @@ async def on_ready():
 async def close():
     act = Activity(name=" to nothing", type=ActivityType.listening)
     await bot.change_presence(activity=act, status=Status.do_not_disturb)
-    await _announcement([750696820736393261], "💤 Sorry, I am going offline.")
+    await _announcement([755715591339376662], "💤 Sorry, I am going offline.")
 
     await bot.change_presence(activity=None, status=Status.offline)
 
